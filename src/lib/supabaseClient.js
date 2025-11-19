7ac94e8bd753369398c42c76012b4ff47c9ad312
SebGonSot <sebasgonsot@gmail.com>
2025-11-19 18:54:50 -0500

    cambio detalle

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://etsldmkjaniejslehqtw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0c2xkbWtqYW5pZWpzbGVocXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1ODY1OTMsImV4cCI6MjA1NDE2MjU5M30.7AGHCvJsR2eRiqCaQRGFhw4XqeIUyaRdDQ7MJ4qItdY'
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
